02e4b6cfb01f59cd90e700d316e3588b73c9a892
Lauren Voswinkel <lvoswink@gmail.com>
2012-09-19 23:52:19 -0400

    Delete the non-believers!

--- lib/log4r/yamlconfigurator.rb
+++
@@ -1,8 +0,0 @@
-class Log4r::YamlConfigurator
-  alias :orig_decode_outputter :decode_outputter
-
-  def self.decode_outputter( op )
-    orig_decode_outputter(op)
-    Outputter[op['name']].default = !!op['default']
-  end
-end
